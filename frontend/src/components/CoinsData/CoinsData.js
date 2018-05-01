23893936a8073e8295ffc5675dcee4e3f6e576bb
Stanislav <ultrastas51@gmail.com>
2018-05-01 15:27:49 +0300

    SCA-14 fix text size in details

--- frontend/src/components/CoinsData/CoinsData.js
+++ frontend/src/components/CoinsData/CoinsData.js
@@ -4,8 +4,8 @@ import CustomTooltip from '../CustomTooltip/CustomTooltip';
 import './CoinsData.css';
 
 const mockData = [
-    {id: 'q1', title: 'Coin Supply', value: '1 billion', tooltip: 'Current amount of coins in circulation'},
-    {id: 'q2', title: 'Block Time', value: '64 seconds', tooltip: 'The time needed for a new block to be generated.<br/> It\'s also the time to send & receive a transaction.<br/> Blocks contain the transaction data and address balances'},
+    {id: 'q1', title: 'Coin supply', value: '1 billion', tooltip: 'Current amount of coins in circulation'},
+    {id: 'q2', title: 'Block time', value: '64 seconds', tooltip: 'The time needed for a new block to be generated.<br/> It\'s also the time to send & receive a transaction.<br/> Blocks contain the transaction data and address balances'},
     {id: 'q3', title: 'Nominal stake interest', value: '1% annual', tooltip: 'Flat APR of generated coins awarded to stakers<br/> relative to the total supply amount of coins.<br/> It\'s also the minimum interest you can get when staking.<br/> The generated coins are awarded through stake rewards of 20 coins'},
     {id: 'q4', title: 'Min transaction fee', value: '0.0001 $BAY', tooltip: 'The minimum viable fee to send a transaction,<br/> paid to the network'},
     {id: 'q5', title: 'Confirmations for approval', value: '10', tooltip: 'Estimated amount of blocks generated to be sure<br/> the transaction has been fully broadcasted and confirmed<br/> by the entire blockchain network'},
